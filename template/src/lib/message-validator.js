5d7754ad551ac9ce68a86228f0ad8e7dffed7d05
Julius Jann <julius.jann@codecentric.de>
2022-10-25 11:12:48 +0200

    Use dirname instead of relativr path to make implementation more stable

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -1,6 +1,6 @@
 const AsyncApiValidator = require('asyncapi-validator');
 
 module.exports.validateMessage = async (payload, channel,name,operation) => {
-  const va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
+  const va = await AsyncApiValidator.fromSource(__dirname + '/../../asyncapi.yaml', {msgIdentifier: 'name'});
   va.validate(name, payload, channel, operation);
 };
